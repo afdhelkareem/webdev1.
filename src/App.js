af95365e297906cdb20b27a72f115e19ede08bc1
afdhelkareem <162284319+afdhelkareem@users.noreply.github.com>
2024-05-23 22:28:12 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>hello, this is afdhel!</h1>
+        <p>i'm 19 years old and live in jakarta</p>
+        <p>My full name is Muhamad Afdhel Kareem Jabbar and I am now studying in Padjadjaran University.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
